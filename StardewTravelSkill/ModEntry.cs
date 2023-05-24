ec5462b37383728399ecc952df898f30c82def38
Achtuur <arthurdegroot88@gmail.com>
2023-05-24 13:58:23 +0200

    Add project files.

---
+++ StardewTravelSkill/ModEntry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewValley;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace StardewTravelSkill
+{
+    internal sealed class ModEntry : Mod
+    {
+        /// <summary>
+        /// Mod entry point, called after mod is first loaded
+        /// </summary>
+        /// <param name="helper">Simplified API for writing mods</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+
+        /*********
+        ** Private methods
+        *********/
+
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            // print button presses to the console window
+            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+        }
+    }
+}
